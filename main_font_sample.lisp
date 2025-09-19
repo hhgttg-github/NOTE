bc07ce0e36202c8244057533626cade53a63b9a1
hhgttg <ksatoshi119@gmail.com>
2025-09-19 11:11:01 +0900

    main_font_sample修正

--- main_font_sample.lisp
+++ main_font_sample.lisp
@@ -6,14 +6,31 @@
     (sdl:fill-surface sdl:*white* :surface sdl:*default-display*)
     (unless (sdl:initialise-default-font *default-font*)
       (error "FONT-EXAMPLE: Cannot initialize the default font."))
-    (sdl:draw-string-solid-* "あめんぼあかいな Text UTF8-Solid 日本語テスト" 0 0
-                             :color sdl:*black*)
-    (sdl:draw-string-shaded-* "あめんぼあかいな Text UTF8-Solid 日本語テスト" 0 32
-                              sdl:*black*
-                              sdl:*yellow*)
-    (sdl:draw-string-blended-* "あめんぼあかいな Text UTF8-Solid 日本語テスト" 0 64
-                               :color sdl:*black*)
+
+;;;; 以下の2つは、*default-font*(*NotoRegular16*)で描かれる。
+    
+    (draw-string "あめんぼあかいな あいうえお" 0 0)
+    (draw-string-* "*** 東京特許許可局 ****" 0 1 sdl:*black* sdl:*white*)
+
+    ;; (sdl:draw-string-solid-* "あめんぼあかいな Text UTF8-Solid 日本語テスト" 0 0
+    ;;                          :color sdl:*black*)
+    ;; (sdl:draw-string-shaded-* "あめんぼあかいな Text UTF8-Solid 日本語テスト" 0 32
+    ;;                           sdl:*black*
+    ;;                           sdl:*yellow*)
+    ;; (sdl:draw-string-blended-* "あめんぼあかいな Text UTF8-Solid 日本語テスト" 0 64
+    ;;                            :color sdl:*black*)
     (sdl:draw-box-* 0 16 64 1 :color sdl:*red*)
+
+;;;; 以下の1つは、*NotoBlack16*で描かれる。sdl:draw-string-*を直接呼びだしている。
+    
+    (sdl:draw-string-solid-* "あめんぼあかいな Text UTF8-Solid 日本語テスト" 0 160
+			     :font *NotoBlack16*
+                             :color sdl:*black*)
+
+    ;; (sdl:draw-string-solid-* "あめんぼあかいな Text UTF8-Solid 日本語テスト" 0 96
+    ;; 			     :font *ttc-font-NotoBlack16*
+    ;;                          :color sdl:*black*)
+    
     (sdl:update-display)    
 
     (sdl:with-events ()
